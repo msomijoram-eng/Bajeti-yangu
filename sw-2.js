72b4d207d5ba377743a1f1bdb2467d37beb0b5dd
msomijoram-eng <msomijoram@gmail.com>
2026-09-02 14:18:11 +0300

    Add files via upload

---
+++ sw-2.js
@@ -0,0 +1,32 @@
+const CACHE = 'bajeti-v5';
+const FILES = ['./'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){ return c.addAll(FILES); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      return r || fetch(e.request).then(function(resp){
+        var rc = resp.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, rc); });
+        return resp;
+      });
+    }).catch(function(){
+      return caches.match('./');
+    })
+  );
+});
